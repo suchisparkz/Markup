6d847284deef62136178d2c53cf3b0c9ae6175a4
Suchitra Manivannan <suchimani@Suchitras-MacBook-Pro.local>
2017-11-15 16:24:22 -0600

    Php File

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+		<head>
+			<title>COMP 436 - Class Exercises</title>
+			<link rel="stylesheet" type="text/css" href="style.css">
+		</head>
+
+		<body>
+			<h1>COMP 436 - Markup Languages</h1>
+			<br>
+			<h3>Class Exercises</h3>
+				<ul>
+					<li><a href="week9.xml"> WEEK 9 </a></li>
+					<li><a href="week10.xml"> WEEK 10 </a></li>
+					<li><a href="week11.xml"> WEEK 11 </a></li>
+				</ul>
+		</body>
+</html>
